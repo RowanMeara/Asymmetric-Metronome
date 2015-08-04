a59e3c5d875857a06d3da811a5aa61348a233ec1
RowanMeara <rrm89@cornell.edu>
2015-08-04 17:24:35 -0500

    Experimentation

--- app/src/main/java/edu/rrm89cornell/asymmetricmetronome/StartStopActivity.java
+++ app/src/main/java/edu/rrm89cornell/asymmetricmetronome/StartStopActivity.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Tutorial extends ActionBarActivity {
+public class StartStopActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutorial);
+        setContentView(R.layout.activity_start_stop);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
+        getMenuInflater().inflate(R.menu.menu_start_stop, menu);
         return true;
     }
 
